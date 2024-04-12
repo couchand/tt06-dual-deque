f49d80619bc3f176a4b18af70fe9dfe2478ea8eb
Andrew Dona-Couch <hi@andrewcou.ch>
2024-04-12 12:39:15 -0400

    fix stack numbering

--- src/dual_stack.v
+++ src/dual_stack.v
@@ -8,10 +8,10 @@
 module dual_stack (
     input  wire       clk,
     input  wire       rst_n,
+    output wire       s0_empty,
+    output wire       s0_full,
     output wire       s1_empty,
     output wire       s1_full,
-    output wire       s2_empty,
-    output wire       s2_full,
     input  wire       stack_select,
     input  wire       push,
     input  wire       pop,
@@ -19,11 +19,26 @@ module dual_stack (
     output wire [7:0] data_out
 );
 
-  wire [7:0] s1_out, s2_out;
-  assign data_out = s1_out | s2_out;
+  wire [7:0] s0_out, s1_out;
+  assign data_out = s0_out | s1_out;
 
   stack #(
     .ADDR(0),
+    .WORDS(16)
+  ) stack0 (
+    .clk(clk),
+    .rst_n(rst_n),
+    .empty(s0_empty),
+    .full(s0_full),
+    .stack_select(stack_select),
+    .push(push),
+    .pop(pop),
+    .data_in(data_in),
+    .data_out(s0_out)
+  );
+
+  stack #(
+    .ADDR(1),
     .WORDS(16)
   ) stack1 (
     .clk(clk),
@@ -35,21 +50,6 @@ module dual_stack (
     .pop(pop),
     .data_in(data_in),
     .data_out(s1_out)
-  );
-
-  stack #(
-    .ADDR(1),
-    .WORDS(16)
-  ) stack2 (
-    .clk(clk),
-    .rst_n(rst_n),
-    .empty(s2_empty),
-    .full(s2_full),
-    .stack_select(stack_select),
-    .push(push),
-    .pop(pop),
-    .data_in(data_in),
-    .data_out(s2_out)
   );
 
 endmodule
